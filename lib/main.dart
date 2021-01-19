dae498c58e3110dc2a81a342413139a6c127e5f5
Brayan Mamani <brayanmamani.developer@gmail.com>
2021-01-18 22:20:48 -0400

    Define layout

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Color(0xFFFCE5C0),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF9AE4AF),
+          title: Text('I Am Poor'),
+        ),
+        body: Center(),
       ),
     ),
   );
